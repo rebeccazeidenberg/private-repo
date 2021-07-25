3e84a5c749c1fd85d5e3a2450532e91d81e8644d
janinacollab <janinacollab@gmail.com>
2021-07-25 19:41:32 -0400

    Adds images to AR_VR page

--- frontend/src/components/pages/AR_VR.js
+++ frontend/src/components/pages/AR_VR.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import ar from './../../img/ar.png'
+import vr from './../../img/vr.png'
 
 const AR_VR = () => {
     return(
-        <div>
-            <p>This is the AR_VR page.</p>
+        <div className="text-center">
+            <img src={ar} className="img-fluid px-4 pt-4 d-block mx-auto" width={700} heigt={700} alt="AR Video" />
+            <img src={vr} className="img-fluid p-4 d-block mx-auto" width={700} heigt={700} alt="VR Video" />
         </div>
     )
 }
