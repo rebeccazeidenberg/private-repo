9a493353339db8e37bace5f9dcb53bc127308e5a
janinacollab <janinacollab@gmail.com>
2021-07-25 20:57:03 -0400

    Adds link to ar page

--- frontend/src/components/pages/AR_VR.js
+++ frontend/src/components/pages/AR_VR.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ar from './../../img/ar.png'
-import vr from './../../img/vr.png'
+import { Link } from 'react-router-dom';
+import ar from './../../img/ar.png';
+import vr from './../../img/vr.png';
 
 const AR_VR = () => {
     return(
         <div className="text-center">
-            <img src={ar} className="img-fluid px-4 pt-4 d-block mx-auto" width={700} heigt={700} alt="AR Video" />
+            <Link to="/ar">
+                <img src={ar} className="img-fluid px-4 pt-4 d-block mx-auto" width={700} heigt={700} alt="AR Video" />
+            </Link>
             <img src={vr} className="img-fluid p-4 d-block mx-auto" width={700} heigt={700} alt="VR Video" />
         </div>
     )
