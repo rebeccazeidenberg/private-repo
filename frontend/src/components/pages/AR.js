052c27b479ffa2b73d387a9446d380f02816818b
Maksim Svetlakov <maksuttt@gmail.com>
2021-08-07 17:45:05 -0400

    NFT tracking to test

--- frontend/src/components/pages/AR.js
+++ frontend/src/components/pages/AR.js
@@ -2,14 +2,34 @@ import React from 'react';
 class AR extends React.Component {
     render() {
         return (
-            
-                <a-scene arjs='sourceType: webcam; debugUIEnabled: false'>
-                    <a-anchor hit-testing-enabled="true">
-                        <a-box position='0 0 0.5' material='opacity: 0.7; color: yellow;'></a-box>
-                    </a-anchor>
-                    <a-camera-static preset="hiro" />
-                </a-scene>
-        
+          <a-scene
+            vr-mode-ui="enabled: false;"
+            renderer="logarithmicDepthBuffer: true;"
+            embedded
+            arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
+          >
+            <a-nft
+              url="https://maksvet.github.io/ar-test/nft/trex"
+              type="nft"
+              smooth="true"
+              smoothCount="10"
+              smoothTolerance=".01"
+              smoothThreshold="5"
+            >
+              <a-entity
+                gltf-model="https://ar-js-org.github.io/AR.js/aframe/examples/image-tracking/nft/trex/scene.gltf"
+                scale="5 5 5"
+                position="50 150 0"
+              ></a-entity>
+            </a-nft>
+            {/* <a-nft
+              markerhandler
+              type="nft"
+              url="https://maksvet.github.io/ar-test/nft/trex"
+            >
+            </a-nft> */}
+            <a-entity camera></a-entity>
+          </a-scene>
         );
     }
 }
