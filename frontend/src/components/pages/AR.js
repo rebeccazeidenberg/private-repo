dbbbecbc08bddba907073df7ef86d1f4885e8abc
Maksim Svetlakov <maksuttt@gmail.com>
2021-08-08 10:00:17 -0400

    NFT tracking working fine

--- frontend/src/components/pages/AR.js
+++ frontend/src/components/pages/AR.js
@@ -1,33 +1,23 @@
 import React from 'react';
+
+
 class AR extends React.Component {
     render() {
         return (
+
           <a-scene
             vr-mode-ui="enabled: false;"
             renderer="logarithmicDepthBuffer: true;"
             embedded
             arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
           >
+        
             <a-nft
-              url="https://maksvet.github.io/ar-test/nft/trex"
-              type="nft"
-              smooth="true"
-              smoothCount="10"
-              smoothTolerance=".01"
-              smoothThreshold="5"
-            >
-              <a-entity
-                gltf-model="https://ar-js-org.github.io/AR.js/aframe/examples/image-tracking/nft/trex/scene.gltf"
-                scale="5 5 5"
-                position="50 150 0"
-              ></a-entity>
-            </a-nft>
-            {/* <a-nft
               markerhandler
               type="nft"
               url="https://maksvet.github.io/ar-test/nft/trex"
             >
-            </a-nft> */}
+            </a-nft>
             <a-entity camera></a-entity>
           </a-scene>
         );
