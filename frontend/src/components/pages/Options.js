c27c40f69e4d403413fe1fa0c86a71a911523e8d
Gareth Graveline-Pleau <gherring@alumni.uoguelph.ca>
2021-08-12 06:56:27 -0400

    hamburger menu made

--- frontend/src/components/pages/Options.js
+++ frontend/src/components/pages/Options.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { stack as Menu } from 'react-burger-menu';
+import { Link } from 'react-router-dom';
+import {FiUser} from 'react-icons/fi';
+import {FaLanguage} from 'react-icons/fa';
+import {BsThreeDots} from 'react-icons/bs';
+import {RiLogoutBoxRLine} from 'react-icons/ri';
+
 
 const Options = () => {
     return(
-        <div>
-            <p>This is the options page.</p>
-        </div>
+        <Menu>
+        <a className="menu-item" style={{ color: '#FFF' }}>
+          <FiUser/>
+        </a>
+        <a className="menu-item" style={{ color: '#FFF' }}>
+          Guest
+        </a>
+        <a className="menu-item" style={{ color: '#FFF' }}>
+          <BsThreeDots/> Change Password
+        </a>
+        <a className="menu-item">
+            <Link to="/languages" style={{ color: '#FFF' }} ><FaLanguage/> Change Language</Link>
+        </a>
+        <a className="menu-item">
+            <Link to="/museums" style={{ color: '#FFF' }} >Museum List</Link>
+        </a>
+        <a className="menu-item">
+            <Link to="/map" style={{ color: '#FFF' }} >Map</Link>
+        </a>
+        <a className="menu-item" style={{ color: '#FFF' }}>
+          <RiLogoutBoxRLine/> Logout
+        </a>
+      </Menu>
     )
 }
 
