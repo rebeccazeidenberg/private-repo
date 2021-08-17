c2904296068db75ff1ebf1b0d2f2ff892a32e5ff
Maksim Svetlakov <maksuttt@gmail.com>
2021-08-17 18:47:25 -0400

    nft added

--- frontend/src/components/pages/AR.js
+++ frontend/src/components/pages/AR.js
@@ -25,9 +25,9 @@ class AR extends React.Component {
 
             <a-nft
               markerhandler
-              name = "trex"
+              name = "modal"
               type="nft"
-              url="NFTs/trex-image-big"
+              url="NFTs/krishna"
             >
             </a-nft>
             
@@ -38,6 +38,64 @@ class AR extends React.Component {
               url="NFTs/download"
             >
             </a-nft>
+
+            <a-nft
+              markerhandler
+              name = "arc"
+              type="nft"
+              url="NFTs/arc"
+            >
+            </a-nft>
+
+            <a-nft
+              markerhandler
+              name = "ganesha"
+              type="nft"
+              url="NFTs/ganesha"
+            >
+            </a-nft>
+
+            <a-nft
+              markerhandler
+              name = "krishna"
+              type="nft"
+              url="NFTs/krishna"
+            >
+            </a-nft>
+
+            <a-nft
+              markerhandler
+              name = "musical_allegory"
+              type="nft"
+              url="NFTs/musical_allegory"
+            >
+            </a-nft>
+
+            <a-nft
+              markerhandler
+              name = "pharaoh"
+              type="nft"
+              url="NFTs/pharaoh"
+            >
+            </a-nft>
+
+            <a-nft
+              markerhandler
+              name = "shark"
+              type="nft"
+              url="NFTs/shark"
+            >
+            </a-nft>
+
+            <a-nft
+              markerhandler
+              name = "starry_night"
+              type="nft"
+              url="NFTs/starry_night"
+            >
+            </a-nft>
+
+            
             <a-entity camera></a-entity>
           </a-scene>
         </>
